41170939123d609a739afc4653774c0093022f84
sirius001php <63809845+sirius001php@users.noreply.github.com>
2020-10-20 17:40:00 +0300

    Update Main.java

--- technology_java_laba1/src/com/CarBody/Main.java
+++ technology_java_laba1/src/com/CarBody/Main.java
@@ -1,9 +1,12 @@
 package com.CarBody;
 
+import java.util.Iterator;
+
 public class Main {
 
     public static void main(String[] args) {
         Car avtopark = new Car("Volkswagen", new Passenger("Golf", "ZFA22300005556777", BodyType.Hatchback, 2019, 1789, 4284, 5, "C"));
+        avtopark.addBody(new Passenger("Golf", "ZFA22300005556777", BodyType.Hatchback, 2019, 1789, 4284, 5, "C"));
         avtopark.addBody(new Passenger("Passat", "KTA22340055556333", BodyType.Sedan, 2015, 1832, 4767, 5, "D"));
         avtopark.addBody(new Passenger("Passat", "ZXC22340123556333", BodyType.Stretch, 2012, 1832, 4867, 5, "D"));
         avtopark.addBody(new Passenger("Golf", "MLA22340200356744", BodyType.Convertible, 2019, 1709, 4084, 4, "C"));
@@ -14,8 +17,10 @@ public class Main {
 //    виводить всю інформацію на екран
     public static void printInfo(Car cars){
         System.out.println("Hi, below you can see some information about the body of different models of the brand " + cars.getMark() + ":");
-        for(int i = 0; i < cars.getSize(); i++){
-            System.out.println(cars.getBoody(i).toString());
+        Iterator iterator = cars.getBodies().iterator();
+        while (iterator.hasNext()) {
+            Boody boody = (Boody) iterator.next();
+            System.out.println(boody.toString());
         }
     }
 }
